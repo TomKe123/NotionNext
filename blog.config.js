7d006ad493a609d76ee22c82554d13a6504ce8c9
TomKe <110755646+TomKe123@users.noreply.github.com>
2024-08-04 13:19:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -353,7 +353,7 @@ const BLOG = {
   COMMENT_TWIKOO_CDN_URL:
     process.env.NEXT_PUBLIC_COMMENT_TWIKOO_CDN_URL ||
     'https://cdn.staticfile.net/twikoo/1.6.17/twikoo.min.js', // twikoo客户端cdn
-
+  CLARITY_ID: process.env.NEXT_PUBLIC_CLARITY_ID || null,
   // utterance
   COMMENT_UTTERRANCES_REPO:
     process.env.NEXT_PUBLIC_COMMENT_UTTERRANCES_REPO || '', // 你的代码仓库名， 例如我是 'tangly1024/NotionNext'； 更多文档参考 https://utteranc.es/
