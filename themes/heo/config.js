6168a99a12dace8a32665b30feb2f218427c4afe
TomKe <110755646+TomKe123@users.noreply.github.com>
2024-09-07 21:31:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'BLOG.TOMSITE.US.KG',
   HEO_HERO_TITLE_4: '新版博客上线',
   HEO_HERO_TITLE_5: 'HEO主题 简约而现代',
-  HEO_HERO_TITLE_LINK: 'https://blog.tomsite.us.kg.com',
+  HEO_HERO_TITLE_LINK: 'https://blog.tomsite.us.kg/changelog',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
